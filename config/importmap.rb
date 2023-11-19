2fcedcb5c24acdce45f09498068dc4b6eab3ac68
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-11-19 11:03:56 +0100

    Fix image import

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "sweetalert", to: "https://ga.jspm.io/npm:sweetalert@2.1.2/dist/sweetalert.min.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "sweetalert2", to: "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.js"
